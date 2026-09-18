8b84f97794b285afe590ef07abb8408b3f46e005
JovanUrosevic7 <jovanurosevic83@gmail.com>
2026-09-18 23:22:05 +0200

    Added 5 dots

--- script.js
+++ script.js
@@ -14,11 +14,14 @@ canvasElement.addEventListener("click", function(e) {
 
   clickPoints.push({x: x, y: y})  
   
-  if(clickPoints.length >= 2){
+  if(clickPoints.length >= 5){
 
     ctx.beginPath()
     ctx.moveTo(clickPoints[0].x, clickPoints[0].y)
     ctx.lineTo(clickPoints[1].x, clickPoints[1].y)
+    ctx.lineTo(clickPoints[2].x, clickPoints[2].y)
+    ctx.lineTo(clickPoints[3].x, clickPoints[3].y)
+    ctx.lineTo(clickPoints[4].x, clickPoints[4].y)
     ctx.stroke()
 
     clickPoints = []
