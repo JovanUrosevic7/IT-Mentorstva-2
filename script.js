ba1e68bfbe7cdad3b5c0295f3d4ae9bde89393ab
JovanUrosevic7 <jovanurosevic83@gmail.com>
2026-09-21 15:30:17 +0200

    Working with api

--- script.js
+++ script.js
@@ -1,56 +1,19 @@
-let clickPoints = []
+fetch("https://dummyjson.com/test")
+  .then(
+    function(response){
+      console.log(response);
+      
+      return response.json()
+    }
 
-let canvasElement = document.getElementById("drawArea")
-let ctx = canvasElement.getContext("2d")
-
-let paragraphElement = document.querySelector("#numberOfLine")
-
-let drawCounter = 0
-
-canvasElement.addEventListener("click", function (e) {
-
-  let rect = canvasElement.getBoundingClientRect()
-
-  let x = e.clientX - rect.left
-  let y = e.clientY - rect.top
-
-  clickPoints.push({ x: x, y: y })
-
-
-
-})
-
-const drawLines = () =>{
-
-  ctx.beginPath()
-  ctx.moveTo(clickPoints[0].x, clickPoints[0].y)
-
-  for (let i = 1; i < clickPoints.length; i++) {
-    ctx.lineTo(clickPoints[i].x, clickPoints[i].y)
-  }
-
-  drawCounter++
-
-  const colorPromt = window.prompt("Unesi boju na engleskom")
-
-  // let randomColor = Math.floor(Math.random() * 16777215).toString(16)
-  // ctx.strokeStyle = "#"+randomColor
-
-  ctx.strokeStyle = colorPromt
-
-  ctx.stroke()
-
-  clickPoints = []
-
-  paragraphElement.innerHTML += `Nacrtana ${drawCounter} linija  #${randomColor}`
-
-}
-
-const reset = () => {
-
-  ctx.clearRect(0, 0, canvasElement.width, canvasElement.height)
-
-}
+  )
+  .then(
+    function(data){
+      console.log(data);
+      
+    }
+    
+  )
 
 
 
