2577e44d941ba5c4728d8443ff781c26fac89396
JovanUrosevic7 <jovanurosevic83@gmail.com>
2026-09-22 23:33:00 +0200

    taking movie api

---
+++ API/script.js
@@ -0,0 +1,21 @@
+const apiUrl = "http://www.omdbapi.com/"
+let apiKey = "450e8dbe"
+
+getMovieByTitle("Home alone")
+
+
+async function getMovieByTitle(title){
+    let response = await callOMDbApi("t="+title)
+    console.log(response);
+    
+}
+
+async function callOMDbApi(params) {
+    const url = apiUrl + "?apiKey=" + apiKey + "&" + params
+
+    const response = await fetch(url)
+    const result = await response.json()
+
+    return result
+
+}
