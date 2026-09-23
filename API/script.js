49346874d8174de72c070d17b63938264c8679b9
JovanUrosevic7 <jovanurosevic83@gmail.com>
2026-09-23 15:34:35 +0200

    Added elements

--- API/script.js
+++ API/script.js
@@ -1,11 +1,37 @@
 const apiUrl = "http://www.omdbapi.com/"
 let apiKey = "450e8dbe"
 
+let movieList = document.querySelector("#movieList")
+
 document.querySelector("#searchBtn").addEventListener("click", async function(){
     let movieName = document.querySelector("#searchMovie").value
     let response = await callOMDbApi(`s=${movieName}`)
 
-    console.log(response);
+    for(let i=0;i<response.Search.length;i++){
+
+        console.log(response.Search[i]);
+        
+        
+        let movieHolder = document.createElement("div")
+        movieHolder.classList = "movieHolder"
+
+        let movieTitle = document.createElement("h3")
+        movieTitle.innerHTML = response.Search[i].Title
+
+        let movieImage = document.createElement("img")
+        movieImage.src = response.Search[i].Poster
+
+        let movieType = document.createElement("p")
+        movieType.innerHTML = response.Search[i].Type
+        
+        let movieYear = document.createElement("p")
+        movieYear.innerHTML = response.Search[i].Year
+        
+        movieHolder.append(movieTitle, movieImage, movieType, movieYear)
+
+        movieList.append(movieHolder)
+        
+    }
     
 })
 
