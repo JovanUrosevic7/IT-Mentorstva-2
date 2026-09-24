67a98153eb358cc2b04ac01fafe7ca05d3f5155c
JovanUrosevic7 <jovanurosevic83@gmail.com>
2026-09-24 13:20:09 +0200

    Added trailer on site

--- API/movie.js
+++ API/movie.js
@@ -7,8 +7,8 @@ const apiKeyTMDB = "9ea24a3810d4f3b8f8401b2fc6644319"
 
 let apiUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKeyTMDB}&append_to_response=videos`
 
-console.log(apiUrl);
-
+let movieTitle = document.querySelector("#movieTitle")
+let trailerContainer = document.querySelector("#trailerContainer")
 
 async function movieTrailer() {
     
@@ -19,9 +19,42 @@ async function movieTrailer() {
 
         console.log(data);
         
+        const videos = data.videos ? data.videos.results : []
+        const trailer = videos.find(v => v.site === "YouTube" && v.type === "Trailer")
+
+        movieTitle.append(data.title)
+        
+
+        if(trailer) {
+            trailerContainer.innerHTML = 
+            `
+                <iframe 
+                    style="border: 1px solid black;"
+                    width="500" 
+                    height="450" 
+                    src="https://www.youtube.com/embed/${trailer.key}" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                </iframe>
+            `
+            
+
+        } else {
+            trailerContainer.innerHTML = "<p>Žao nam je, trejler za ovaj film nije dostupan.</p>";
+
+        }
+            
+
+        
+
 
     } catch(error) {
         console.error("Doslo je do greske prilikom ucitavanja trailera", error);
+        console.log(`https://www.youtube.com/embed/${trailer.key}`);
+        console.log(`https://www.youtube.com/embed/${trailer.key}`);
+
         
 
     }
